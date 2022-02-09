017c3ca9582591dce0e393d4885503fd3e6a4e75
eldenbra <github@michaelgruschke.de>
2022-02-09 23:57:14 +0100

    undo button

--- app/src/main/java/com/example/ultimateplaybyplaytracker/feature_tracker/presentation/tracker/TrackerScreen.kt
+++ app/src/main/java/com/example/ultimateplaybyplaytracker/feature_tracker/presentation/tracker/TrackerScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -53,17 +54,20 @@ fun TrackerScreen(
         },
         scaffoldState = scaffoldState
     ) {
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+        ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(Color.White),
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 Text(
                     text = playState.plays.takeLast(5).map { item ->
                         item.event
-                    }.joinToString(">"),
+                    }.joinToString(" > "),
                     color = Color.Black
                 )
 
